7ab0f880f572d8a4d0d0c2cc3733102187c9c5e0
mariomartinezjr <mariomartinezjr@gmail.com>
2017-10-04 16:00:09 -0400

    teste branch

---
+++ fabricaweb2/src/main/java/br/com/fabricadeprogramador/model/teste.java
@@ -0,0 +1,13 @@
+package br.com.fabricadeprogramador.model;
+
+public class teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		String teste;
+	}
+
+}
